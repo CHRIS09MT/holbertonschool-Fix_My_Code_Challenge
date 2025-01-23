d8ab20224658a2c4f8c71ec91ccd8b0bb546d80c
CHRIS09MT <christophermoralestorres06@gmail.com>
2025-01-23 14:42:16 -0500

    ready?

--- challenge/4-delete_dnodeint/print_dlistint.c
+++ challenge/4-delete_dnodeint/print_dlistint.c
@@ -7,16 +7,16 @@
  * @h: A pointer to the first element of a list
  *
  * Return: The number of elements printed
- */
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t n = 0;  // Initialize the count of printed elements
+    size_t n = 0;
 
-    while (h)  // Traverse the list until the end
+    while (h)
     {
-        printf("%d\n", h->n);  // Print the current node's value
-        h = h->next;  // Move to the next node
-        n++;  // Increment the count
+        printf("%d\n", h->n);
+        h = h->next;
+        n++;
     }
-    return n;  // Return the total count of printed elements
+    return n;
 }
